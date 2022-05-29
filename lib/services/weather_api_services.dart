ae6d1dbe66d3d989061f1f3c83dac66bba7445b1
hyuem <uhh0701@gmail.com>
2022-05-29 14:37:45 +0900

    ch 67. Weather API Services 2

--- lib/services/weather_api_services.dart
+++ lib/services/weather_api_services.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import "package:http/http.dart" as http;
 import 'package:weather_provider/constants/constants.dart';
 import 'package:weather_provider/exceptions/weather_exception.dart';
+import 'package:weather_provider/models/weather.dart';
 import 'package:weather_provider/services/http_error_handler.dart';
 
 class WeatherApiServices {
@@ -25,7 +26,7 @@ class WeatherApiServices {
     try {
       final http.Response response = await http.get(uri);
       if (response.statusCode != 200) {
-        throw httpErrorHandler(response);
+        throw Exception(httpErrorHandler(response));
       }
 
       final responseBody = json.decode(response.body);
@@ -40,6 +41,30 @@ class WeatherApiServices {
       }
 
       return responseBody[0]["woeid"];
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Weather> getWeather(int woeid) async {
+    final Uri uri = Uri(
+      scheme: "https",
+      host: kHost,
+      path: "/api/location/$woeid",
+    );
+
+    try {
+      final http.Response response = await http.get(uri);
+      if (response.statusCode != 200) {
+        throw Exception(httpErrorHandler(response));
+      }
+
+      final weatherJson = json.decode(response.body);
+      final Weather weather = Weather.fromJson(weatherJson);
+
+      print(weather);
+
+      return weather;
     } catch (e) {
       rethrow;
     }
